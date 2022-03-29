0f7ec85390fc449930203f35af7d1197faf9b232
rekayno <92271828+rekayno@users.noreply.github.com>
2022-03-29 15:38:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import string
+import random
+from typing import Any
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+
+TOKEN = os.getenv('TOKEN')
+dp = Dispatcher()
+
+characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
+
+@dp.message(commands={"start"})
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f'Hello, <b>{message.from_user.full_name}!</b>\nI can generate a password for you\nPlease send me the password length by message')
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> Any:
+    if message.text.isnumeric():
+        length = int(message.text)
+        if length > 1000:
+            await message.answer('Maximum password length - 1000 characters')
+            return
+        await message.answer(generate_password(int(message.text)))
+        
+
+def main() -> None:
+    bot = Bot(TOKEN, parse_mode='HTML')
+    dp.run_polling(bot)
+
+def generate_password(length: int):
+    password = []
+    for i in range(int(length)):
+        password.append(random.choice(characters))
+    random.shuffle(password)
+    return ''.join(password)
+
+
+
+if __name__ == '__main__':
+    main()
